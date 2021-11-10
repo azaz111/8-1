e59d2de7a66d9d07ce5caafd542c2e1962125935
azaz111 <86608104+azaz111@users.noreply.github.com>
2021-11-10 10:06:56 +0300

    Update bash.bash

--- bash.bash
+++ bash.bash
@@ -32,8 +32,12 @@ sleep 2
 # Третий этап -----------------------------------------------------
 cd
 chmod 777 trans.sh
+chmod 777 Copi1.sh
+chmod 777 Copi.sh
 mkdir /aws32 
 screen -dmS mount rclone mount --daemon aws32: /aws32 
+mkdir /baws32 
+screen -dmS mount rclone mount --daemon baws32: /baws32 
 # Четвкртый --------------------------------------------------
 cd
 # Монтируем диск 1
@@ -121,7 +125,10 @@ screen -dmS otpravka_na_serv ./otpravka.sh
  
 ./dir_akka.sh
 # ЗАпуск Плотера ------------------------------
-screen -dmS videorender1 ./chia-plotter/build/chia_plot -n -1 -r 8 -u 256 -t /disk1/vid1/ -2 /disk2/vid2/ -d /disk3/video/ -f b8e1d57e3e2dbb40ac8f2b257b762d05fcfc5b79c32a22255424644b7d183daa7c454624783f2d959c02eb1d2a4ba3a3 -p 91ea997633345082b15f83b957449180037030b6b7485f07ed4ee7558d08d3efbccf2c3d68ba724f5b3a8281a0055e27
+screen -dmS Copi1 ./Copi1.sh
+screen -dmS Copi ./Copi.sh
+screen -dmS videorender1 ./chia-plotter/build/chia_plot -n -1 -r 16 -u 256 -t /disk1/vid1/ -2 /disk2/vid2/ -d /disk3/video/ -f b8e1d57e3e2dbb40ac8f2b257b762d05fcfc5b79c32a22255424644b7d183daa7c454624783f2d959c02eb1d2a4ba3a3 -p 91ea997633345082b15f83b957449180037030b6b7485f07ed4ee7558d08d3efbccf2c3d68ba724f5b3a8281a0055e27
+screen -dmS videorender2 ./chia-plotter/build/chia_plot -n -1 -r 16 -u 256 -t /disk1/vid1/ -2 /disk2/vid2/ -d /disk3/video1/ -f b8e1d57e3e2dbb40ac8f2b257b762d05fcfc5b79c32a22255424644b7d183daa7c454624783f2d959c02eb1d2a4ba3a3 -p 91ea997633345082b15f83b957449180037030b6b7485f07ed4ee7558d08d3efbccf2c3d68ba724f5b3a8281a0055e27
 screen -dmS trans ./trans.sh
 screen -dmS otchet python3 awsstat.py
 screen -r trans
